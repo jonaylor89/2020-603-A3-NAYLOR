7f9fd6594262845430ce043054a2f90f71c0a4e1
John Naylor <jonaylor89@gmail.com>
2020-10-17 23:34:24 -0400

    maevon

--- KNN.java
+++ KNN.java
@@ -1,10 +1,59 @@
 
 class KNN {
 
+    public static void setup() {
 
+        /*
+         * 
+         * create CD reducer matrix of size (size(testSet * k))
+         * 
+         * Classes can be random but the distances should be set to infinity
+         */
+
+    }
+
+    public static void map() {
+        /*
+         *
+         * for i in range(testSet)
+         *      ComputeKNN(dataPoint, i, TrainSetSplitj, k)
+         *      for n in range(0, k)
+         *          CDj[i][n] = (class of neighbor n, distance of neighbor n)
+         * 
+         * key = idMapper
+         * context.write(key, CDj)
+         */
+    }
+
+    public static void reduce() {
+
+        /*
+         *
+         * for i in range(testSet)
+         *      cont = 0
+         *      for i in range(0, k)
+         *          if CDj[i][cont].dist < CD_reducer[i][n].dist
+         *              CD_reducer[i][n] = CDj[i][cont]
+         *              cont++
+         * 
+         */
+
+    }
+
+    public static void cleanup() {
+
+        /*
+         *
+         * for i in range(0, size(testSet))
+         *      PredClass[i] = majorityVoting(classes(CD_reducer))
+         *      key = i
+         *      context.write(key, PredClass[i])
+         *
+         */
+
+    }
 
     public static void main(String[] argv) {
-
 
     }
 }
