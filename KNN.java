e00bdbe1c9dfe15c60e6785b268ecb024126485d
John Naylor <jonaylor89@gmail.com>
2020-10-27 15:59:22 -0400

    pls

--- KNN.java
+++ KNN.java
@@ -6,7 +6,10 @@ import java.io.FileReader;
 import java.io.File;
 import java.io.DataInput;
 import java.io.DataOutput;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 import java.util.StringTokenizer;
 import java.util.List;
 import java.util.HashMap;
@@ -262,7 +265,18 @@ public class KNN {
 
     private static String getTestInstances(String filename) {
 
-        return Files.readString(filename);
+        StringBuilder contentBuilder = new StringBuilder();
+ 
+        try (Stream<String> stream = Files.lines(Paths.get(filename), StandardCharsets.UTF_8)) 
+        {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        }
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+ 
+        return contentBuilder.toString();
     }
 
     public static void main(String[] argv) throws Exception {
